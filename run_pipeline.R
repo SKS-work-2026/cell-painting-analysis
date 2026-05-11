771034422966c7e8d3155714c48cc836092a8d10
SKS-work-2026 <shashikanta.work@gmail.com>
2026-05-11 16:36:48 +0530

    Delete run_pipeline.R

--- run_pipeline.R
+++
@@ -1,8 +0,0 @@
-source("config/parameters.R")
-
-source("scripts/01_load_data.R")
-source("scripts/02_preprocess.R")
-source("scripts/03_mito_scoring.R")
-source("scripts/04_analysis_plots.R")
-
-cat("✅ FULL PIPELINE COMPLETED\n")
